87b17edc53ac7de35aa66f3128f6ec4ecbf7d4d3
pcnerd <ajayihabeeb977@gmail.com>
2026-01-30 13:42:00 +0100

    🔧 chore: setup postgres database config and dependency

---
+++ src/app/core/db/database.py
@@ -0,0 +1,32 @@
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from app.core.config import settings
+
+
+class Base(DeclarativeBase, MappedAsDataclass):
+    pass
+
+DATABASE_URI = settings.DB_URI
+DATABASE_PREFIX = settings.DB_PREFIX
+
+DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
+
+async_engine = create_async_engine(
+    url=DATABASE_URL,
+    future=True
+)
+
+async_session = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False
+)
+
+async def async_get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
